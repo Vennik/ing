cc3b84b035f34e54109df62431d6ef1f2f2ba862
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2014-11-12 21:41:56 +0100

    Move client to the client folder. configure server to display the static pages

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -20,9 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
-app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
